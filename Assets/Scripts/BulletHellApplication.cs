b355a9984eb86bad1665af44887accf49f8e1f7f
Michael Odermatt <43145047+MichaelOdermatt@users.noreply.github.com>
2022-03-27 12:37:52 -0600

    added source to framework approach

--- Assets/Scripts/BulletHellApplication.cs
+++ Assets/Scripts/BulletHellApplication.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Link to MVC approach outline https://www.toptal.com/unity-unity3d/unity-with-mvc-how-to-level-up-your-game-development
+/// </summary>
 public class BulletHellApplication : MonoBehaviour
 {
 
