f4d8169eea3e6027c6ad5b8103d8d3881e9728ae
Michael Odermatt <43145047+MichaelOdermatt@users.noreply.github.com>
2022-03-29 21:36:15 -0600

    added enemy base class

--- Assets/Scripts/Controllers/BasicEnemyController.cs
+++ Assets/Scripts/Controllers/BasicEnemyController.cs
@@ -2,6 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BasicEnemyController : BulletHellElement 
+public class BasicEnemyController : EnemyControllerBase 
 {
 }
