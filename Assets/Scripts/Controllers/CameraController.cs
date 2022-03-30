31290412aa63fcb5b2a8426633ed05103010145b
Michael Odermatt <43145047+MichaelOdermatt@users.noreply.github.com>
2022-03-29 21:13:30 -0600

    added camera XZ rotation on movement

--- Assets/Scripts/Controllers/CameraController.cs
+++ Assets/Scripts/Controllers/CameraController.cs
@@ -8,6 +8,9 @@ public class CameraController : BulletHellElement
     private CameraModel cameraModel;
     private CameraView cameraView;
 
+    private float horizontal;
+    private float vertical;
+
     private float cameraBobHeight;
     private float cameraBobSpeed;
     private float cameraBobTimer;
@@ -34,6 +37,18 @@ public class CameraController : BulletHellElement
 
     private void Update()
     {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+
+        // set camera XZ rotation when an input is given
+        Quaternion cameraTarget = Quaternion.Euler(cameraDefaultRotation.x + maxCameraRotationAngleX * vertical * -1 , 
+            cameraDefaultRotation.y, 
+            maxCameraRotationAngleZ * horizontal * -1);
+
+        cameraView.transform.rotation = Quaternion.Slerp(cameraView.transform.rotation, 
+            cameraTarget, 
+            cameraRotationSpeed * Time.deltaTime);
+
         // bob the camera up and down
         cameraBobTimer += cameraBobSpeed * Time.deltaTime;
         cameraView.transform.position = new Vector3(cameraDefaultPosition.x, 
