d53d3143c847addb58bf3177a110b7f52e0529b9
Michael Odermatt <43145047+MichaelOdermatt@users.noreply.github.com>
2022-04-12 19:32:12 -0600

    added update method for enemy fire

--- Assets/Scripts/BulletHellNotification.cs
+++ Assets/Scripts/BulletHellNotification.cs
@@ -7,5 +7,6 @@ public class BulletHellNotification
     public const string PlayerBulletOnInvisible = "PlayerBullet.OnInvisible";
     public const string PlayerBulletOnCollision = "PlayerBullet.OnCollision";
     public const string BasicEnemyOnCollision = "BasicEnemy.OnCollision";
+    public const string BasicEnemyOnFire = "EnemyBullet.OnFire";
     public const string EnemyBulletOnInvisible = "EnemyBullet.OnInvisible";
 }
--- Assets/Scripts/Views/BasicEnemyView.cs
+++ Assets/Scripts/Views/BasicEnemyView.cs
@@ -4,9 +4,21 @@ using UnityEngine;
 
 public class BasicEnemyView : BulletHellElement 
 {
+    private double lastShootTime;
 
     private void OnCollisionEnter(Collision collision)
     {
         app.Notify(BulletHellNotification.BasicEnemyOnCollision, this, collision, this.gameObject);   
+    }
+
+    void Update()
+    {
+        if (lastShootTime + app.modelContainer.basicEnemyModel.fireRate > Time.time)
+        {
+            return;
+        }
+        lastShootTime = Time.time;
+
+        app.Notify(BulletHellNotification.BasicEnemyOnFire, this, gameObject);
     }
 }
